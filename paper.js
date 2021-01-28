ff654c3cbd8417e08afbd64bc5398e836ab56940
pallavi980 <77010202+pallavi980@users.noreply.github.com>
2021-01-28 17:12:28 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,27 @@
+class Paper {
+    constructor(x, y,radius) {
+
+      var options={
+        isStatic:true,
+        restitution:0.3,
+        friction:0.5,
+        density:1.2
+        
+      }
+      
+     this.body= Matter.Bodies.circle(x,y,radius);
+      
+      
+      
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      
+      fill("pink");
+      circle(pos.x,pos.y, this.width, this.height);
+      ellipse(CENTER);
+    }
+  };
+  
